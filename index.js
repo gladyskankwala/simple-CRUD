11b019ab012f3dbb28f091eb19ce209c92208ef7
Gladis <gladyskankwala03@gmail.com>
2026-04-10 21:09:08 +0200

    database connection

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config()
 const app = express()
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port 3000");
+
+mongoose.connect(process.env.MONGO_URL)
+ .then(() => console.log("Mongodb connected"))
+ .catch((err) => console.log("Connection failed", err))
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running port ${process.env.PORT}`)
 })
 
-app.get('/', (req, res) =>{
-    res.send("you are ugly")
+app.get('/', (req, res) => {
+    res.send("subscribe-")
 })
