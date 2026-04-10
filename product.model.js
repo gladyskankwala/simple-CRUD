ab400e8aadf7ac61317c1858764d6213fbca0c68
Gladis <gladyskankwala03@gmail.com>
2026-04-10 22:01:50 +0200

    mongodb schema

---
+++ product.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please enter product name"],
+    },
+    quantity: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    Image: {
+      type: String,
+      require: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Product = mongoose.model("Product", ProductSchema)
+
+module.exports = Product;
