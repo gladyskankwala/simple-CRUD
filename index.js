a1984a91943c802126c5583d18280cacc67669f3
Gladis <gladyskankwala03@gmail.com>
2026-04-11 22:48:13 +0200

    ProductScheme

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ require("dotenv").config()
 const app = express()
 
 
+
+app.get('/', (req, res) => {
+    res.send("subscribe-")
+})
+
+app.post('api/products', (req, res) => {
+    res.send("Data received")
+})
+
 mongoose.connect(process.env.MONGO_URL)
  .then(() => console.log("Mongodb connected"))
  .catch((err) => console.log("Connection failed", err))
 
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running port ${process.env.PORT}`)
-})
-
-app.get('/', (req, res) => {
-    res.send("subscribe-")
 })
