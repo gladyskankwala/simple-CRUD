49dbc8eae0795eded8a6663010fe0a250d1328be
Gladis <gladyskankwala03@gmail.com>
2026-04-12 23:16:32 +0200

    fix the diplay product error

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.get('/', (req, res) => {
 })
 
 
+app.get( '/api/products' , async (req, res) => {
+    try {
+        const  products = await Products.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Products.create(req.body)
